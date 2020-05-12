95323f4edd19d8e55d179cc98805547c43255cc1
Chris Griffing <cmgriffing@gmail.com>
2020-05-12 16:11:21 -0700

    ugh: moar

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -47,12 +47,12 @@ const installExtensions = async () => {
 };
 
 const createWindow = async () => {
-  if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.DEBUG_PROD === 'true'
-  ) {
-    await installExtensions();
-  }
+  // if (
+  //   process.env.NODE_ENV === 'development' ||
+  //   process.env.DEBUG_PROD === 'true'
+  // ) {
+  await installExtensions();
+  // }
 
   mainWindow = new BrowserWindow({
     show: false,
