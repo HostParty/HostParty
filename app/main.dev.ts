f1d5633fc16b70f39ad2565652fd01619edf96b5
Chris Griffing <cmgriffing@gmail.com>
2020-05-20 10:24:28 -0700

    fix: allow callback js to fire

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -130,14 +130,9 @@ const createWindow = async () => {
     show: false,
     width: 1366,
     height: 1100,
-    webPreferences:
-      process.env.NODE_ENV === 'development' || process.env.E2E_BUILD === 'true'
-        ? {
-            nodeIntegration: true
-          }
-        : {
-            preload: path.join(__dirname, 'dist/renderer.prod.js')
-          }
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
   // const node = require('child_process').fork(
--- app/server/get-callback-html.js
+++ app/server/get-callback-html.js
@@ -2,12 +2,13 @@ function getCallbackHtml() {
   return `
 <html>
 <head>
-
 </head>
 <body>
   <h1>Redirecting...</h1>
   <div class="debug"></div>
   <script>
+  try {
+
     const debugElement = document.querySelector('.debug');
 
     const keyPairs = window.location.hash.substr(1)
@@ -16,7 +17,7 @@ function getCallbackHtml() {
 
 
 
-      const accessToken = keyPairs.find(keyPair => keyPair[0] === 'access_token')[1];
+    const accessToken = keyPairs.find(keyPair => keyPair[0] === 'access_token')[1];
 
     debugElement.innerText = JSON.stringify(accessToken);
 
@@ -24,6 +25,9 @@ function getCallbackHtml() {
       window.opener.postMessage(accessToken, '*')
       window.close();
     }
+  } catch(e) {
+    document.write(JSON.stringify(e));
+  }
   </script>
 </body>
 </html>
